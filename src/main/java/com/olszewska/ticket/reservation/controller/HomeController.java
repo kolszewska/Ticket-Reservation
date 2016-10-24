7aa5239f8993709581c065554a13f5460c594691
kolszewska <k.olszewskaaa@gmail.com>
2016-10-24 13:09:37 +0200

    Initial commit

---
+++ src/main/java/com/olszewska/ticket/reservation/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.olszewska.ticket.reservation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value="/")
+    public String home() {
+        return "main.jsp";
+    }
+}
