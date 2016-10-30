974219e4c4e86fe07a6bf0fb3c7ea400f62f0087
kolszewska <k.olszewskaaa@gmail.com>
2016-10-30 22:01:29 +0100

    Added new taglib for displaying date in reservation view

---
+++ src/main/java/com/olszewska/ticket/reservation/taglib/DateTag.java
@@ -0,0 +1,24 @@
+package com.olszewska.ticket.reservation.taglib;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+public class DateTag extends SimpleTagSupport {
+
+    private Timestamp date;
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+    public void doTag() throws JspException,IOException {
+        if(date != null) {
+            String formattedDate = new SimpleDateFormat("YYYY-MM-dd HH:mm").format(date);
+            JspWriter out = getJspContext().getOut();
+            out.println(formattedDate);
+        }
+    }
+}
