26243aff3b71aa629e6a456c57b9a98544a343a4
kolszewska <k.olszewskaaa@gmail.com>
2016-11-02 23:00:33 +0100

    Final commit

--- src/main/java/com/olszewska/ticket/reservation/service/ReservationService.java
+++ src/main/java/com/olszewska/ticket/reservation/service/ReservationService.java
@@ -4,5 +4,6 @@ import com.olszewska.ticket.reservation.model.Reservation;
 
 public interface ReservationService {
     Reservation findById(int id);
+
     Reservation findByVerificationKey(String verificationKey);
 }
